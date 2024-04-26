152307bc5ef8279844831625b5e20e61c640ef84
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:19:38 +0300

    add Example007

---
+++ Example007/Program.cs
@@ -0,0 +1,7 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.Clear();
+
+int number = new Random().Next(100, 999);
+int result = number / 10 % 10;
+Console.WriteLine(number);
+Console.WriteLine(result);
