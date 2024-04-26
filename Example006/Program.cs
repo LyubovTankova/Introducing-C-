143606472b2314e90d7068af375d47aefdbd56d6
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:19:28 +0300

    add Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,7 @@
+﻿//Показать последнюю цифру трёхзначного числа
+Console.Clear();
+
+int number = new Random().Next(100, 999);
+int result = number % 10;
+Console.WriteLine(number);
+Console.WriteLine(result);
