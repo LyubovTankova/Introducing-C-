412b357ad6fb6c8b79cb0f378c64a931bd3afd00
Liubov <lyubasik85@rambler.ru>
2024-04-26 19:56:34 +0300

    add Example048

---
+++ Example048/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу показывающую первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+string ShowNumbers(int a, int b, int n)
+{
+    if (n >= 0) return $"{a} " + ShowNumbers(b, a + b, n - 1);
+    return string.Empty;
+}
+Console.Clear();
+Console.Write("Введите первое число:  ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число:  ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+int n = new Random().Next(5, 10);
+Console.WriteLine(ShowNumbers(a, b, n));
