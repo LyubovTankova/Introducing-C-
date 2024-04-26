c8aaf1bdeecfa7268c305c385725d4dc08ff7657
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:19:18 +0300

    add Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿//Показать четные числа от 1 до N
+Console.Clear();
+
+Console.WriteLine("Enter a number: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+while (i <= n)
+{
+    if (i % 2 == 0)
+        Console.WriteLine(i);
+    i++;
+}
+Console.WriteLine();
