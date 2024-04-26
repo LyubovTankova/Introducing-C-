b90240ba858634f30a7b484c88232e26cacdfd03
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:20:06 +0300

    add Example009

---
+++ Example009/Program.cs
@@ -0,0 +1,14 @@
+﻿//Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 999);
+int number2 = 0;
+
+int numberDel()
+{
+    number2 = number / 100 * 10 + number % 10;
+    return number2;
+}
+numberDel();
+
+Console.WriteLine(number);
+Console.WriteLine(number2);
