1d7d0f320b255d5bc7ea9197a3fbc3c2fcbfe121
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:20:42 +0300

    add Example013

---
+++ Example013/Program.cs
@@ -0,0 +1,29 @@
+﻿// По двум заданным числам проверить является ли одно квадратом другого
+Console.Clear();
+
+int a;
+int b;
+Console.WriteLine("Введите первое число: ");
+a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число: ");
+b = int.Parse(Console.ReadLine() ?? "0");
+{
+    if (a == b * b)
+    {
+        Console.WriteLine("Первое число является квадратом второго");
+    }
+    else
+    {
+        Console.WriteLine("Первое число не является квадратом второго");
+    }
+
+    if (b == a * a)
+    {
+        Console.WriteLine("Второе число является квадратом первого");
+    }
+    else
+    {
+        Console.WriteLine("Второе число не является квадратом первого");
+    }
+}
+Console.WriteLine();
