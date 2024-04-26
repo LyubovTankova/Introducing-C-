fcf3077fe9b47423d84733e27485a382e12e4565
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:19:45 +0300

    add Example008

---
+++ Example008/Program.cs
@@ -0,0 +1,23 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 99);
+int max = 0;
+int maxDigit()
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if (firstDigit > secondDigit)
+    {
+        max = firstDigit;
+    }
+    else
+    {
+        max = secondDigit;
+    }
+
+    return max;
+}
+maxDigit();
+Console.WriteLine(number);
+Console.Write("Наибольшая цифра числа: ");
+Console.WriteLine(max);
