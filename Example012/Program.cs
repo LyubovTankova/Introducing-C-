dce5553f592d9e68839edb4de0df571cf4fd9c1e
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:20:31 +0300

    add Example012

---
+++ Example012/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Clear();
+
+int a;
+Console.WriteLine("Введите число дня недели : ");
+a = int.Parse(Console.ReadLine() ?? "0");
+{
+    if (a <= 5)
+    {
+        Console.WriteLine("Рабочий день!");
+    }
+    if (a >= 6)
+    {
+        Console.WriteLine("Выходной день!");
+    }
+}
+Console.WriteLine();
