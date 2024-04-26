1c5d78237bccf74a614309cf753031a176029637
Liubov <lyubasik85@rambler.ru>
2024-04-26 19:01:24 +0300

    add Example017

---
+++ Example017/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write($"Сумма цифр числа {n} равна ");
+int sumDigits = 0;
+
+while (n > 0)
+{
+    int digit = n % 10;
+    sumDigits = sumDigits + digit;
+    n = n / 10;
+}
+Console.WriteLine(sumDigits);
