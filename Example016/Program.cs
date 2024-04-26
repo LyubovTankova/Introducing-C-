bbd2e9aa63eec9a00cc15f3e21fded1b4b21e9b9
Liubov <lyubasik85@rambler.ru>
2024-04-26 19:01:15 +0300

    add Example016

---
+++ Example016/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти кубы чисел от 1 до n
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int cube3;
+for (int i = 1; i <= n; i++)
+{
+    cube3 = i * i * i;
+    Console.WriteLine($"Куб числа {i} = {cube3}");
+}
+
+Console.WriteLine();
