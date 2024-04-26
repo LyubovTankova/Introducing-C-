fc6a5ef03725534162b331945b40cc3a8249bdee
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:19:09 +0300

    add Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,15 @@
+﻿//Выяснить является ли число чётным
+Console.Clear();
+
+int a;
+Console.WriteLine("Введите число: ");
+a = int.Parse(Console.ReadLine() ?? "0");
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+Console.WriteLine();
