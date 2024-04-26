79144e2fdb2947950d5c2d3107ca5bed18794a36
Liubov <lyubasik85@rambler.ru>
2024-04-26 19:01:33 +0300

    add Example018

---
+++ Example018/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до n
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int work = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    work = work * i;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {n} равно {work}");
