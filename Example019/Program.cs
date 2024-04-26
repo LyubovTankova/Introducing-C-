22e73160a70eccb105d21f6fae8572c86c907941
Liubov <lyubasik85@rambler.ru>
2024-04-26 19:01:46 +0300

    add Example019

---
+++ Example019/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+
+int n = new Random().Next(1, 10);
+Console.WriteLine($"Число {n}");
+int i = 1;
+int cub3;
+
+while (i <= n)
+{
+    cub3 = i * i * i;
+    if (cub3 % 2 == 0)
+        Console.WriteLine($"{i} = {cub3} заканчивается на четную цифру");
+    i++;
+}
+Console.WriteLine();
