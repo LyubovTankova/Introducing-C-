e6f21463b563556c04a0a86629208ff3e2217fef
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:20:21 +0300

    add Example011

---
+++ Example011/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int number = new Random().Next(1, 1200);
+Console.WriteLine(number);
+
+if(number / 100 > 0)
+   {
+    Console.WriteLine(number / 100 % 10);
+   }
+else
+   {
+    Console.WriteLine("У числа нет третьей цифры");
+   }
+Console.WriteLine();
