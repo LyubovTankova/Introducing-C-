7568c72cd4e9dc246c1cfaf84df141b54cf63c22
Liubov <lyubasik85@rambler.ru>
2024-04-26 19:02:23 +0300

    add Example024

---
+++ Example024/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+Console.Clear();
+
+int size = 10;
+
+int[] CountArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(10, 99);
+    }
+    return arr;
+}
+void Print(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        if (i % 2 > 0) 
+        sum = sum + array[i];
+    }
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел одномерного массива стоящих на нечетной позиции равна {sum}");
+}
+
+Print(CountArray(size));
