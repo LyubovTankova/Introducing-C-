35a9390cff4ae062d3e9e516ce2f9bd290976a7b
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:20:59 +0300

    add Example015

---
+++ Example015/Program.cs
@@ -0,0 +1,23 @@
+﻿﻿// Найти расстояние между точками в пространстве 2D/3D
+Console.Clear();
+
+double xA, xB, yA, yB, zA, zB, distance;
+
+Console.WriteLine("Расстояние между двумя точками в пространстве: ");
+Console.WriteLine("Введите координаты xA: ");
+xA = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите координаты xB: ");
+xB = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите координаты yA: ");
+yA = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите координаты yB: ");
+yB = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите координаты zA: ");
+zA = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите координаты zB: ");
+zB = double.Parse(Console.ReadLine() ?? "0");
+distance = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2));
+
+Console.WriteLine();
+Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
+Console.WriteLine();
