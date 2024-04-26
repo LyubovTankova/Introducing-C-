6d4a54047257dc6629548f89c3bcaa70ff44b3a9
Liubov <lyubasik85@rambler.ru>
2024-04-26 18:18:58 +0300

    add Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,13 @@
+﻿//Найти максимальное из трех чисел
+Console.Clear();
+
+int a = 1;
+int b = 20;
+int c = 60;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
