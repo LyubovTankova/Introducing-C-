2f36d4b79c03d39a68cf7ba88256b0a83d7d1f1b
Liubov <lyubasik85@rambler.ru>
2024-04-26 19:25:52 +0300

    add Example036

---
+++ Example036/Program.cs
@@ -0,0 +1,17 @@
+﻿// Реализовать функцию, которая проверяет является ли заданное число n полиндромом
+Console.Clear();
+
+bool IsPalindrome(string a)
+{
+    for (int i = 0; i < a.Length / 2; i++)
+        if (a[i] != a[a.Length - i - 1])
+        {
+            return false;
+        }
+    return true;
+}
+
+Console.Write("Введите число или слово: ");
+string a = Console.ReadLine();
+if (IsPalindrome(a)) Console.WriteLine($"{a} является палиндромом");
+else Console.WriteLine($"{a} не является палиндромом");
