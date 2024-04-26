1ada6536664d7273f7a52f76f0c27a2c85bc61ea
Liubov <lyubasik85@rambler.ru>
2024-04-26 19:15:41 +0300

    add Example027

---
+++ Example027/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+Console.Clear();
+
+double[,] TablArray(int m, int n)
+{
+    double[,] tabl = new double[m, n];
+    for (int i = 0; i < tabl.GetLength(0); i++)
+    {
+        for (int j = 0; j < tabl.GetLength(1); j++)
+        {
+            tabl[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return tabl;
+}
+void PrintTabl(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "   ");
+        }
+    Console.WriteLine();   
+    }    
+}
+
+int m = 5;
+int n = 5;
+double[,] tabl = TablArray(m, n);
+PrintTabl(tabl);
